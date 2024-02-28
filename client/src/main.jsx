0bd7539ed747719d0350d96beb2310ef362faed8
kigongo-vincent <kigongovincent81@gmail.com>
2024-02-28 00:44:21 -0800

    added content

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import { AnimatePresence } from 'framer-motion'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <AnimatePresence mode='popLayout'>
     <App />
+    </AnimatePresence>
     </BrowserRouter>
   </React.StrictMode>,
 )
--- client/src/pages/SplashScreen.jsx
+++ client/src/pages/SplashScreen.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import {motion} from 'framer-motion'
 const SplashScreen = () => {
   return (
-    <div>
-      splash screen
-    </div>
+    <motion.div 
+    initial={{x: 100}}
+    animate={{x: 0}}
+    exit={{x: 100}}
+    className='full-page splash-screen d-flex align-items-center justify-content-between flex-column'>
+      <motion.div/>
+      <motion.div className='d-flex align-items-center justify-content-center  flex-column'>
+      <img src="https://mla7c53pmfhs.i.optimole.com/wW1pluc-hwydQ3-J/w:996/h:1024/q:mauto/https://athealthblog.com/wp-content/uploads/2022/02/medical-g2012fccfe_1280.png" alt="" />
+      <h1 className="poppins-bold mt-4">
+        Hospital Management System
+      </h1>
+      </motion.div>
+      <p className="opacity-50">All rights reserved hospitalmanagementsystem &copy;</p>
+    </motion.div>
   )
 }
 
